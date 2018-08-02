71dd7b998ffd9fc89417d370ee00ee7a04a9ff44
Freddy Marsden <freddy.Marsden@gmail.com>
2018-08-02 16:08:57 +0100

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+import requests
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from random import randint
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('copy paste this in your browser to authorize bot https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=0'.format(os.environ['CLIENTID']))
+    print('------')
+
+@client.event
+async def on_message(message):
+  
+
+client.run(os.environ['TOKEN'])
